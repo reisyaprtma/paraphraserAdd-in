171df9cdd8a41e544fe149d4ef6d38fbf0fb8111
reisyaprtma <d1041221032@student.untan.ac.id>
2026-04-04 11:41:31 +0700

    fix mode parafrase cepat

--- Test/src/taskpane/taskpane.js
+++ Test/src/taskpane/taskpane.js
@@ -1002,8 +1002,8 @@ function setupEventListeners() {
     };
 
     // 2. Paraphrase Action [cite: 457]
-    document.getElementById("paraphrase-btn").onclick = sendPrompt('normal');
-    document.getElementById("paraphrase-fast-btn").onclick = sendPrompt('fast');
+    document.getElementById("paraphrase-btn").onclick = () => sendPrompt('normal');
+    document.getElementById("paraphrase-fast-btn").onclick = () => sendPrompt('fast');
     // document.getElementById("test-btn").onclick = fetchFailedTest;
     // 3. Insert Action [cite: 470]
     document.getElementById("insert-btn").onclick = () => {
